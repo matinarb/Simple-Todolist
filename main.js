23ec8e08822ff42848d3e5f719b0ff6d576c6cb1
matinarb <mr.matin.a80@gmail.com>
2024-08-17 02:17:40 +0430

    add edit

--- main.js
+++ main.js
@@ -1,49 +1,86 @@
 var items = document.querySelector('.items')
 var btn = document.querySelector('.submit-button');
 var input = document.querySelector('.input')
+var editMsg = document.querySelector('.edit-msg-container')
+var editBtn = document.querySelector('#edit-btn')
+var cancelBtn = document.querySelector('#cancel-btn')
+var editInput = document.querySelector('#edit-input')
+var msg = document.querySelector('.msg')
 
+//add note
 btn.addEventListener('click', addNote);
 function addNote(e) {
     e.preventDefault()
     let count = rank()
-    let txt=input.value
+    let txt = input.value
 
-    let msg = document.querySelector('.msg')
     msg.classList.add('no-msg')
 
     let item = document.createElement('div')
     item.classList.add('item')
     let number = document.createElement('div')
     number.classList.add('number')
-    number.innerHTML=`${count}`
+    number.innerHTML = `${count}`
     let note = document.createElement('p')
     note.classList.add('note')
-    note.innerHTML=`${txt}`
+    note.innerHTML = `${txt}`
+    let edit = document.createElement('button')
+    edit.classList.add('edit')
+    edit.innerHTML = 'edit'
     let dbtn = document.createElement('button')
     dbtn.classList.add('delete')
-    dbtn.innerHTML='X'
+    dbtn.innerHTML = 'X'
     item.appendChild(number)
     item.appendChild(note)
+    item.appendChild(edit)
     item.appendChild(dbtn)
     items.appendChild(item)
 
-    input.value=''
-
-    note.addEventListener('click', done)
-    function done(){
-        item.classList.toggle('done')
-    }
-
-    
-    dbtn.addEventListener('click', remove)
-    function remove(){
-        let deletedItem= dbtn.parentElement
-        items.removeChild(deletedItem)
-        if(rank()==1){
-            msg.classList.remove('no-msg')
-        }
-    }
+    input.value = ''
 }
+
+//edit note
+items.addEventListener('click', (e) => {
+    if (e.target.className == 'edit') {
+        editMsg.style.display = 'flex'
+        let firstNote = e.target.parentElement.children[1]
+        editInput.value = firstNote.textContent
+        editBtn.addEventListener('click', (e) => {
+            firstNote.innerHTML = editInput.value
+            firstNote =''
+            editMsg.style.display = 'none'
+        })
+        cancelBtn.addEventListener('click', (e) => {
+            firstNote =''
+            editMsg.style.display = 'none'
+        })
+    }
+})
+
+delete note
+items.addEventListener('click', (e) => {
+    if (e.target.className == 'delete') {
+        if (confirm('Are You Sure To Delete This Note?')) {
+            e.target.parentElement.remove()
+            if (items.childElementCount == 1) {
+                msg.classList.remove('no-msg')
+            }
+        }
+        else{
+
+        }
+
+    }
+})
+
+//done note
+items.addEventListener('click', (e) => {
+    if (e.target.className == 'note') {
+        e.target.parentElement.classList.toggle('done')
+    }
+})
+
+// ranking
 function rank() {
     let count = items.childElementCount
     return count
